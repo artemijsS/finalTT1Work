90e91659609e7e5cf10ed2a25f0aae4bfb252327
artemijsS <artjom1408@gmail.com>
2020-12-26 17:22:09 +0200

    26/12/2020

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const db =
     {
         "MD": {
-            "t-shirt": [
+            "t-shirts": [
                 {
                     "id": 0,
                     "name": "Snow?",
@@ -46,14 +46,14 @@ const db =
                     "priceEUR": 43.00,
                     "sale": "10%",
                     "finalPriceEUR": 38.70,
-                    "img": "https://merchdose.com/pictures/c34d-mocaforafitejomumyfy.jpg",
+                    "img": "https://merchdose.com/pictures/c34d-betapubigu.jpg",
                     "description": "lorem ipsum",
                     "rating": 65
                 }
             ]
         },
         "ProBro": {
-            "t-shirt": [
+            "t-shirts": [
                 {
                     "id": 5,
                     "name": "Мой райончик, мои правила",
@@ -77,7 +77,7 @@ const db =
             ]
         },
         "YanaBruk": {
-            "t-shirt": [
+            "t-shirts": [
                 {
 
                 }
@@ -89,3 +89,85 @@ const db =
             ]
         }
     };
+const types = ["t-shirts", "hoodies"];
+
+const products = [
+    {
+        "id": 0,
+        "partners": ["MD", "All"],
+        "name": "Snow?",
+        "types": ["T-shirts", "All"],
+        "priceEUR": 12.00,
+        "sale": null,
+        "img": "https://merchdose.com/pictures/c34d-kosomiluku.jpg",
+        "description": "lorem ipsum",
+        "rating": 80
+    },
+    {
+        "id": 1,
+        "partners": ["MD", "All"],
+        "name": "Stay out of it",
+        "types": ["T-shirts", "All"],
+        "priceEUR": 16.00,
+        "sale": null,
+        "img": "https://merchdose.com/pictures/c34d-tyvyfosuranaruhakyro.jpg",
+        "description": "lorem ipsum",
+        "rating": 50
+    },
+    {
+        "id": 2,
+        "partners": ["MD", "All"],
+        "name": "Art",
+        "types": ["T-shirts", "All"],
+        "priceEUR": 25.00,
+        "sale": null,
+        "img": "https://merchdose.com/pictures/c34d-dopecomijevehyjomaho.jpg",
+        "description": "lorem ipsum",
+        "rating": 40
+    },
+    {
+        "id": 3,
+        "partners": ["MD", "All"],
+        "name": "Christmas chaos 2",
+        "types": ["T-shirts", "All"],
+        "priceEUR": 17.00,
+        "sale": null,
+        "img": "https://merchdose.com/pictures/c34d-mocaforafitejomumyfy.jpg",
+        "description": "lorem ipsum",
+        "rating": 45
+    },
+    {
+        "id": 4,
+        "partners": ["MD", "All"],
+        "name": "Reflektor",
+        "types": ["Hoodies", "All"],
+        "priceEUR": 43.00,
+        "sale": "10%",
+        "finalPriceEUR": 38.70,
+        "img": "https://merchdose.com/pictures/c34d-betapubigu.jpg",
+        "description": "lorem ipsum",
+        "rating": 65
+    },
+    {
+        "id": 5,
+        "partners": ["ProBro", "All"],
+        "name": "Мой райончик, мои правила",
+        "types": ["T-shirts", "All"],
+        "priceEUR": 16.00,
+        "sale": null,
+        "img": "https://merchdose.com/pictures/c34d-dyforesili.jpg",
+        "description": "lorem ipsum",
+        "rating": 49
+    },
+    {
+        "id": 6,
+        "partners": ["ProBro", "All"],
+        "name": "Чё как брать поживает?",
+        "types": ["Hoodies", "All"],
+        "priceEUR": 42.50,
+        "sale": null,
+        "img": "https://merchdose.com/pictures/c34d-rubycogoso.jpg",
+        "description": "lorem ipsum",
+        "rating": 50
+    }
+]
--- index.js
+++ index.js
@@ -1,23 +1,142 @@
+let actualState = {
+    language: "EN",
+    currency: "EUR",
+    filterType: "All",
+    filterPartner: "All",
+    sortBy: "rating",
+    search: ""
+}
 
 
+window.addEventListener('DOMContentLoaded', () => {
+    const partners = Object.keys(db);
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const data = db;
-    console.log(data)
-
-    let actualState = {
-        language: "en",
-        currency: "eur"
-    }
-
-
+    showProducts();
 
     //FILTERS
-    $filterType = $('#type');
-    $filetrPartner = $('#partner');
 
+    const $filterType = $('#type');
+    const $filterPartner = $('#partner');
+
+    $filterType.on('change', () => {
+        actualState.filterType = $filterType.val();
+        showProducts();
+    })
+    $filterPartner.on('change', () => {
+        actualState.filterPartner = $filterPartner.val();
+        showProducts();
+    })
 
     //SORT POP UP
-    $popUp = $('.sort-pop-up');
+
+    const $sortBySpan = $('#sort-by span');
+    const $popUp = $('.sort-pop-up');
+    const $popUpLiActive = $('.sort-pop-up ul li');
+    const sortBy = document.getElementById('sort-by');
+
+    document.getElementById('rating').onclick = setSortBy;
+    document.getElementById('price (low)').onclick = setSortBy;
+    document.getElementById('price (height)').onclick = setSortBy;
+    document.getElementById('alphabet').onclick = setSortBy;
+
+    const popUpToggle = () => {
+        if ($popUp.css("display") === "none") {
+            $popUp.css("display", "block");
+            $('#sort-by svg').css("transform", "rotate(0)");
+        }
+        else {
+            $popUp.css("display", "none");
+            $('#sort-by svg').css("transform", "rotate(180deg)");
+        }
+    }
+
+    document.addEventListener('click', (event) => {
+        if (!sortBy.contains(event.target)) {
+            $popUp.css("display", "none");
+            $('#sort-by svg').css("transform", "rotate(180deg)");
+        }
+    });
+
+    $sortBySpan.on('click', () => {
+        popUpToggle()
+    })
+
+    function setSortBy () {
+        actualState.sortBy = this.id;
+        popUpToggle();
+        $sortBySpan.html(this.id)
+        $popUpLiActive.removeClass();
+        document.getElementById(this.id).classList.add("active");
+        showProducts();
+    }
+
+    // $popUpLi.click(() => {
+    //     console.log(this.id)
+    // })
+
     // $popUp.css("display", "none");
+
+    //SEARCH
+    const $search = $('#product-search')
+
+    $search.on('keypress', () => {
+        if ($search.val().length > 1) {
+            actualState.search = $search.val().toLowerCase();
+            showProducts();
+        }
+        // console.log($search.val().length)
+        if ($search.val().length < 1) {
+
+        }
+        console.log(11111111)
+    })
+
+
+    function showProducts() {
+        const $products = $('#products');
+        $products.html('test');
+
+        let items = [];
+        let productCards = '';
+
+        if (actualState.search) {
+            products.map(obj => {
+                if (obj.partners.includes(actualState.filterPartner)) {
+                    if (obj.types.includes(actualState.filterType)) {
+                        if (obj.name.toLowerCase().includes(actualState.search)) {
+                            items.push(obj);
+                        }
+                    }
+                }
+            })
+        }
+        else {
+            products.map(obj => {
+                if (obj.partners.includes(actualState.filterPartner)) {
+                    if (obj.types.includes(actualState.filterType)) {
+                        items.push(obj);
+                    }
+                }
+            })
+        }
+
+        console.log(items)
+
+        items.map(obj => {
+            productCards +=
+                `<div class="product-block">
+                    <div class="product-img">
+                        <img src=${obj.img} alt="error">
+                    </div>
+                    <h4>${obj.name}</h4>
+                    <div class="label">${obj.partners[0]}</div>
+                    <div class="product-bottom">
+                        <div class="price">${actualState.currency === "EUR" ? obj.priceEUR : "--"} €</div>
+                        <button class="button">select options</button>
+                    </div>
+                </div>`
+        })
+
+        $products.html(productCards);
+    }
 });
