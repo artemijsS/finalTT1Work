243d2daeff643ca80643a1a53fff8f0e4ea83119
artemijsS <artjom1408@gmail.com>
2020-12-26 12:58:38 +0200

    26/12/2020

---
+++ db.js
@@ -0,0 +1,91 @@
+const db =
+    {
+        "MD": {
+            "t-shirt": [
+                {
+                    "id": 0,
+                    "name": "Snow?",
+                    "priceEUR": 12.00,
+                    "sale": null,
+                    "img": "https://merchdose.com/pictures/c34d-kosomiluku.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 80
+                },
+                {
+                    "id": 1,
+                    "name": "Stay out of it",
+                    "priceEUR": 16.00,
+                    "sale": null,
+                    "img": "https://merchdose.com/pictures/c34d-tyvyfosuranaruhakyro.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 50
+                },
+                {
+                    "id": 2,
+                    "name": "Art",
+                    "priceEUR": 25.00,
+                    "sale": null,
+                    "img": "https://merchdose.com/pictures/c34d-dopecomijevehyjomaho.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 40
+                },
+                {
+                    "id": 3,
+                    "name": "Christmas chaos 2",
+                    "priceEUR": 17.00,
+                    "sale": null,
+                    "img": "https://merchdose.com/pictures/c34d-mocaforafitejomumyfy.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 45
+                }
+            ],
+            "hoodies": [
+                {
+                    "id": 4,
+                    "name": "Reflektor",
+                    "priceEUR": 43.00,
+                    "sale": "10%",
+                    "finalPriceEUR": 38.70,
+                    "img": "https://merchdose.com/pictures/c34d-mocaforafitejomumyfy.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 65
+                }
+            ]
+        },
+        "ProBro": {
+            "t-shirt": [
+                {
+                    "id": 5,
+                    "name": "Мой райончик, мои правила",
+                    "priceEUR": 16.00,
+                    "sale": null,
+                    "img": "https://merchdose.com/pictures/c34d-dyforesili.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 49
+                }
+            ],
+            "hoodies": [
+                {
+                    "id": 6,
+                    "name": "Чё как брать поживает?",
+                    "priceEUR": 42.50,
+                    "sale": null,
+                    "img": "https://merchdose.com/pictures/c34d-rubycogoso.jpg",
+                    "description": "lorem ipsum",
+                    "rating": 50
+                }
+            ]
+        },
+        "YanaBruk": {
+            "t-shirt": [
+                {
+
+                }
+            ],
+            "hoodies": [
+                {
+
+                }
+            ]
+        }
+    };
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
 
 
-
 window.addEventListener('DOMContentLoaded', (event) => {
+    const data = db;
+    console.log(data)
+
+    let actualState = {
+        language: "en",
+        currency: "eur"
+    }
+
+
+
+    //FILTERS
+    $filterType = $('#type');
+    $filetrPartner = $('#partner');
+
 
     //SORT POP UP
     $popUp = $('.sort-pop-up');
-    $popUp.css("display", "none");
+    // $popUp.css("display", "none");
 });
