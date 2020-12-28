d634b3e3d263ddaa64473aeed5113dd14811e4d5
artemijsS <artjom1408@gmail.com>
2020-12-28 16:03:05 +0200

    28/12/2020

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ let actualState = {
     search: "",
     cart: {
         items: {},
-        totalPriceEUR: 0,
-        totalPriceRUB: 0,
+        totalPrices: {
+            EUR: 0,
+            RUB: 0
+        },
         totalCount: 0
     }
 }
 
+//PRODUCT CARD
 function productCard(id) {
 
     const item = products[id];
@@ -122,9 +125,11 @@ function productCard(id) {
         const cartItem = {
             id: item.id,
             name: item.name,
-            img: item.img,
-            priceEUR: item.prices.EUR,
-            priceRUB: item.prices.RUB,
+            img: $('#product-card-img').attr('src'),
+            prices: {
+                EUR: item.prices.EUR,
+                RUB: item.prices.RUB
+            },
             size: $size.val(),
             color: $color.val()
         }
@@ -163,14 +168,98 @@ function productCard(id) {
 
         // actualState.cart.items.push(cartItem)
         actualState.cart.totalCount += 1;
-        actualState.cart.totalPriceEUR += cartItem.priceEUR;
-        actualState.cart.totalPriceRUB += cartItem.priceRUB;
-        console.log(actualState.cart)
+        actualState.cart.totalPrices.EUR += cartItem.prices.EUR;
+        actualState.cart.totalPrices.RUB += cartItem.prices.RUB;
+        // console.log(actualState.cart)
 
         $(".popup").fadeOut();
         $productBox.html('');
         $(".popup").css("visibility", "");
     })
+}
+
+
+//CART BOX
+function cartBox () {
+
+
+
+    console.log(actualState.cart);
+    let tmp;
+    let cartItems = '';
+    Object.keys(actualState.cart.items).map(id => {
+        Object.keys(actualState.cart.items[id]).map(size => {
+            Object.keys(actualState.cart.items[id][size]).map(color => {
+                console.log(actualState.cart.items[id][size][color])
+                cartItems +=
+                    `
+                    <div class="cart-item">
+                        <div class="cart-item-img">
+                            <img src=${actualState.cart.items[id][size][color].id.img} alt="error">
+                        </div>
+                        <div class="name-params">
+                            <div class="name">${actualState.cart.items[id][size][color].id.name}</div>
+                            <div class="params">${size + ', ' + color}</div>
+                        </div>
+                        <div class="quantity-price">
+                            <div class="quantity">
+                                <div class="button button-circle">
+                                    <svg width="13" height="13" viewBox="0 0 10 10" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                                d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                                                fill="red"/>
+                                    </svg>
+                                </div>
+                                <b>${actualState.cart.items[id][size][color].totalItemCount}</b>
+                                <div class="button button-circle">
+                                    <svg width="13" height="13" viewBox="0 0 10 10" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                                d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                                                fill="#EB5A1E"/>
+                                        <path
+                                                d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                                                fill="red"/>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="total-item-price">${actualState.cart.items[id][size][color].id.prices[actualState.currency] + ' ' + actualState.currencyS[actualState.currency]}</div>
+                        </div>
+                    </div>
+                    `
+            })
+        })
+    })
+    // language=HTML
+    tmp =
+        `
+            <div class="popup-cart-box">
+                <div class="cart-box">
+                    <span id="closeCart" class="close">X</span>
+                    <div class="cart-head">CART</div>
+                    <div class="cart-items">
+                        ${cartItems}
+                    </div>
+                    <div class="cart-footer">
+                        <div class="totalCount">Quantity: ${actualState.cart.totalCount}</div>
+                        <div class="totalPrice">${actualState.cart.totalPrices[actualState.currency] + ' ' + actualState.currencyS[actualState.currency]}</div>
+                        <button class="button">BUY</button>
+                    </div>
+                </div>
+            </div>
+        `
+    $('#cartBox').html(tmp);
+    $('.popup-cart-box').fadeIn();
+    $('.popup-cart-box').css('visibility', 'visible')
+
+    $('#closeCart').click(function() {
+        $(".popup-cart-box").fadeOut();
+        $('#cartBox').html('');
+        $(".popup-cart-box").css("visibility", "")
+    });
+
+
 }
 
 
@@ -180,7 +269,8 @@ window.addEventListener('DOMContentLoaded', () => {
     showProducts();
 
 
-    //HEADER currency toggle
+    //HEADER
+    // currency toggle
     const $currencyLi = $('#currency li');
     const $currencySpan = $('#currency Span');
 
@@ -192,6 +282,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
         console.log(actualState.currency)
         showProducts();
+    })
+    // cart
+    $('#cart').on('click', () => {
+        cartBox();
     })
 
 
